8119dea8716343498c69ca482ff3d86c74783202
Herb <i@4leaf.me>
2014-10-30 01:50:35 +0800

    modify some text

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # icons
 
-generate icons and config file (e.g. Contents.json) required by app in ios or android, inspired by http://makeappicon.com
+generate icons and config file (e.g. Contents.json) required by iOS or Android app, inspired by http://makeappicon.com
 
 ## web version
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(name='icons',
       version='1.0',
       description='generate icons and config file (e.g. Contents.json) ' +
-                  'required by app in ios or android, inspired by ' +
+                  'required by iOS or Android app, inspired by ' +
                   'http://makeappicon.com',
       url='http://github.com/exherb/icons',
       author='Herb Brewer',
